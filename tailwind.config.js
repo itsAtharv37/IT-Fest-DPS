32e6cda22158b568488d6d0b0bff6085395b74c4
AaTherf <it.atharv.m39@gmail.com>
2023-11-04 00:20:58 +0530

    did sm stuff basic design ntg else

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./IT-FEST/**/*.{html,js}"],
+  // content: ["./IT-FEST/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
+
